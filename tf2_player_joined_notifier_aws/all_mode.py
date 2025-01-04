1006484a060061ab16c7f2ddc19f2ebf15e420d2
rkawano518 <rkawano518@gmail.com>
2025-01-04 14:40:53 -0800

    Add additional logging for ALL mode

--- tf2_player_joined_notifier_aws/all_mode.py
+++ tf2_player_joined_notifier_aws/all_mode.py
@@ -98,9 +98,10 @@ def all_mode() -> dict:
                 print(f"Finished updating database with player \"{name}\". Response: {response}")
 
     if not should_notify:
+        print("Notification doesn't need to be sent")
         return generate_return_message(SUCCESS_STATUS_CODE, "Don\'t need to notify")
 
-    print("Sending notification")
+    print(f"Notification needs to be sent. Sending notifications for: {pending_player_names}")
     subject = f"{EMAIL_SUBJECT_PREFIX}Player has joined the server"
     message = format_server_info_to_string(server_name=server_name,
                                            player_count=player_count,
